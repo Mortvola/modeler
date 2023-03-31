b6bfd8eff52265b000557161e944170b2324cf01
Mortvola <mortvola@gmail.com>
2023-03-31 11:07:09 -0600

    Added list of textures in the textures tab and added a button to flip a texture on disk.

--- Terrain4/UserInterface/Textures/TexturesView.swift
+++ Terrain4/UserInterface/Textures/TexturesView.swift
@@ -10,20 +10,55 @@ import MetalKit
 
 struct TexturesView: View {
     @State private var openFile = false
+    @State var selectedItem: URL? = nil
     
     var body: some View {
-        Button {
-            openFile = true
-        } label: {
-            Image(systemName: "photo")
-        }
-        .fileImporter(isPresented: $openFile, allowedContentTypes: [.image]) { result in
-            if let url = try? result.get() {
-                try? importFile(srcUrl: url)
+        VStack {
+            HStack {
+                Button {
+                    openFile = true
+                } label: {
+                    Image(systemName: "square.and.arrow.down")
+                }
+                .fileImporter(isPresented: $openFile, allowedContentTypes: [.image]) { result in
+                    if let url = try? result.get() {
+                        try? importFile(srcUrl: url)
+                    }
+                }
+                Button {
+                    if let url = selectedItem {
+                        try? flipTexutre(url: url)
+                    }
+                } label: {
+                    Image(systemName: "arrow.up.arrow.down")
+                }
             }
+            List {
+                ForEach(getTextures(), id: \.absoluteString) { url in
+                    Button {
+                        selectedItem = url
+                    } label: {
+                        HStack {
+                            Text("\(url.lastPathComponent)")
+                            Spacer()
+                        }
+                    }
+                    .selected(selected: url == selectedItem)
+                }
+            }
+            Spacer()
         }
     }
     
+    func getTextures() -> [URL] {
+        let dir = getDocumentsDirectory().appending(path: "/textures")
+        let urls = try? FileManager.default.contentsOfDirectory(at: dir, includingPropertiesForKeys: [])
+        
+        return urls?.sorted { a, b in
+            a.lastPathComponent < b.lastPathComponent
+        } ?? []
+    }
+
     func importFile(srcUrl: URL) throws {
         let fileName = srcUrl.lastPathComponent
         
@@ -89,6 +124,24 @@ struct TexturesView: View {
                     try? pngData.write(to: fileUrl)
                 }
             }
+        }
+    }
+    
+    func flipTexutre(url: URL) throws {
+        do {
+            let data = try Data(contentsOf: url)
+            if let image = UIImage(data: data), let cgImage = image.cgImage {
+                var ciImage = CIImage(cgImage: cgImage)
+                    .transformed(by: CGAffineTransform(scaleX: 1.0, y: -1.0))
+                let t2 = UIImage(ciImage: ciImage)
+                    
+                if let pngData = t2.pngData() {
+                    try pngData.write(to: url)
+                }
+            }
+        }
+        catch {
+            print(error)
         }
     }
 }
